129c411c89badc9ef062c1932d144e3871d6fdad
Keeler Mendoza <keelerbe@gmail.com>
2019-09-07 16:30:49 +0100

    update firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,14 @@
-import firebase from 'firebase'
+import * as firebase from 'firebase/app'
 import 'firebase/database'
 import 'firebase/auth'
 
 const config = {
-  apiKey: "AIzaSyC26lrJChGjfiwcZmDwDGycScjdZT3cxSc",
-  authDomain: "shopping-cart-78b0a.firebaseapp.com",
-  databaseURL: "https://shopping-cart-78b0a.firebaseio.com",
-  projectId: "shopping-cart-78b0a",
-  storageBucket: "shopping-cart-78b0a.appspot.com",
-  messagingSenderId: "466014896978"
+	apiKey: 'AIzaSyC26lrJChGjfiwcZmDwDGycScjdZT3cxSc',
+	authDomain: 'shopping-cart-78b0a.firebaseapp.com',
+	databaseURL: 'https://shopping-cart-78b0a.firebaseio.com',
+	projectId: 'shopping-cart-78b0a',
+	storageBucket: 'shopping-cart-78b0a.appspot.com',
+	messagingSenderId: '466014896978'
 }
 
 firebase.initializeApp(config)
@@ -16,7 +16,4 @@ firebase.initializeApp(config)
 const database = firebase.database()
 const auth = firebase.auth()
 
-export {
-  database,
-  auth
-}
+export { database, auth }
